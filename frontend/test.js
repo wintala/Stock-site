7b3f09988e65a216bafa3267021b5158079640c5
Waltteri Rintala <rintala.waltteri@gmail.com>
2020-08-27 17:36:48 +0300

    second commit

--- frontend/test.js
+++
@@ -1,9 +0,0 @@
-const exampleList = [[2, 3, 1], [3, 2, 1], [4, 5, 1]]
-
-let sumList = []
-
-sumList = [...Array(exampleList[0].length).keys()].map(i => exampleList.map(l => l[i]).reduce((a, b) => (a + b)))
-
-
-
-
